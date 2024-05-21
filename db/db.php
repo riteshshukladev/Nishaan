b88696c6f6636118fdcebef4ef09b597f5849199
RItesh <shuklaritesh5678@gmail.com>
2024-05-21 14:58:40 +0530

    DB Migration success postgresql -> Mysql

--- db/db.php
+++ db/db.php
@@ -1,15 +1,13 @@
 <?php
-
 $host = 'localhost';
 $db = 'bookmarks';
-$user = 'postgres';
-$pass = '89999';
-$dsn = "pgsql:host=$host;dbname=$db;";
+$user = 'root'; 
+$pass = ''; 
+$dsn = "mysql:host=$host;dbname=$db;";
 $options = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
 ];
-
 try {
     $pdo = new PDO($dsn, $user, $pass, $options);
 } catch (PDOException $e) {
